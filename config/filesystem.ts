c139c07b74c20910545be4a3fb5975624ba7134a
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2025-01-12 16:03:10 +0530

    fix filesystem config

--- config/filesystem.ts
+++ config/filesystem.ts
@@ -2,48 +2,47 @@ import { configNamespace, StorageOptions } from '@intentjs/core';
 
 export default configNamespace(
   'filesystem',
-  (): StorageOptions =>
-    ({
-      /**
-       * -----------------------------------------------------
-       * Default Storage Disk
-       * -----------------------------------------------------
-       * Documentation - https://tryintent.com/docs/file-storage
-       *
-       * This value is the name of the default disk. This will be
-       * used when you use the `Storage` facade to access your
-       * files.
-       */
+  (): StorageOptions => ({
+    /**
+     * -----------------------------------------------------
+     * Default Storage Disk
+     * -----------------------------------------------------
+     * Documentation - https://tryintent.com/docs/file-storage
+     *
+     * This value is the name of the default disk. This will be
+     * used when you use the `Storage` facade to access your
+     * files.
+     */
 
-      default: process.env.DEFAULT_STORAGE || 'local',
+    default: process.env.DEFAULT_STORAGE || 'local',
 
-      /**
-       * -----------------------------------------------------
-       * Storage Disks
-       * -----------------------------------------------------
-       *
-       * Here you can configure all your different storage disks.
-       * A default configuration has been added for your application.
-       * You can change the disk that you would want to use at the runtime.
-       *
-       * Storage Driver: "local", "s3"
-       */
-      disks: {
-        local: {
-          driver: 'local',
-          basePath: 'storage/uploads',
-        },
-
-        /**
-         *  s3: {
-         *    driver: 's3',
-         *    region: process.env.AWS_REGION,
-         *    bucket: process.env.S3_BUCKET,
-         *    accessKey: process.env.AWS_ACCESS_KEY, // either use access key / secret key or the credentials
-         *    secretKey: process.env.AWS_SECRET_KEY,
-         *    credentials: fromIni({ profile: process.env.AWS_PROFILE }),
-         *  },
-         */
+    /**
+     * -----------------------------------------------------
+     * Storage Disks
+     * -----------------------------------------------------
+     *
+     * Here you can configure all your different storage disks.
+     * A default configuration has been added for your application.
+     * You can change the disk that you would want to use at the runtime.
+     *
+     * Storage Driver: "local", "s3"
+     */
+    disks: {
+      local: {
+        driver: 'local',
+        basePath: 'storage/uploads',
       },
-    }) as StorageOptions,
+
+      /**
+       *  s3: {
+       *    driver: 's3',
+       *    region: process.env.AWS_REGION,
+       *    bucket: process.env.S3_BUCKET,
+       *    accessKey: process.env.AWS_ACCESS_KEY, // either use access key / secret key or the credentials
+       *    secretKey: process.env.AWS_SECRET_KEY,
+       *    credentials: fromIni({ profile: process.env.AWS_PROFILE }),
+       *  },
+       */
+    },
+  }),
 );
