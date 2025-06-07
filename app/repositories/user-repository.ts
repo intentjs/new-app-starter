6310e6fd83335a0639e81becc131f8a75b43041a
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2025-06-07 17:58:07 +0530

    Refactor UserDbRepository to simplify model declaration by removing the @InjectModel decorator and directly assigning the UserModel.

--- app/repositories/user-repository.ts
+++ app/repositories/user-repository.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@intentjs/core";
 import { UserModel } from "#models/user";
-import { DatabaseRepository, InjectModel } from "@intentjs/core/db";
+import { DatabaseRepository } from "@intentjs/core/db";
 
 @Injectable()
 export class UserDbRepository extends DatabaseRepository<UserModel> {
-  @InjectModel(UserModel)
-  declare model: UserModel;
+  model = UserModel;
 }
